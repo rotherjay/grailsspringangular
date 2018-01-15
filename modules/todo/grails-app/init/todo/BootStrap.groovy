3ab610147554427ab2d37fadd17b1e26835f03e8
rut <rother@asti.dost.gov.ph>
2018-01-15 16:47:30 +0800

    initial commit

---
+++ modules/todo/grails-app/init/todo/BootStrap.groovy
@@ -0,0 +1,13 @@
+package todo
+
+import todo.Todo
+
+class BootStrap {
+
+    def init = { servletContext ->
+        5.times { new Todo(description: "Todo ${it+1}").save() }
+    }
+    
+    def destroy = {
+    }
+}
