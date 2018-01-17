f0dcd83d1e57673041345b83db098139432daeaf
rotherjay <rotherjacopino@gmail.com>
2018-01-17 22:42:26 +0800

    updated todo.gson

--- module_003/todo/grails-app/domain/todo/Todo.groovy
+++ module_003/todo/grails-app/domain/todo/Todo.groovy
@@ -3,7 +3,7 @@ package todo
 
 import grails.rest.*
 
-@Resource(readOnly = false, formats = ['json', 'xml'])
+// @Resource(readOnly = false, formats = ['json', 'xml'])
 class Todo {
     String description
     boolean done
